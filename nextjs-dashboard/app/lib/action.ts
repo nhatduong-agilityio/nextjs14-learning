7ad8a109e35367a37f70b3027a7f35e549f67816
Nhat Cong Duong <nhat.duong@asnet.com.vn>
2024-08-15 09:27:39 +0700

    Adding server side validation

--- nextjs-dashboard/app/lib/action.ts
+++ nextjs-dashboard/app/lib/action.ts
@@ -1,70 +1,95 @@
-"use server";
+'use server'
 
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
-import { z } from "zod";
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { z } from 'zod'
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["pending", "paid"]),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
-});
+})
 
-const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const CreateInvoice = FormSchema.omit({ id: true, date: true })
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
-    customerId: formData.get("customerId"),
-    amount: formData.get("amount"),
-    status: formData.get("status"),
-  });
+export type State = {
+  errors?: {
+    customerId?: string[]
+    amount?: string[]
+    status?: string[]
+  }
+  message?: string | null
+}
 
-  const amountInCents = amount * 100;
-  const date = new Date().toISOString().split("T")[0];
+export async function createInvoice(prevState: State, formData: FormData) {
+  // Validate form using Zod
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  })
 
-  try {
-    await console.log(formData);
-  } catch (error) {
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
     return {
-      message: "Database Error: Failed to Create Invoice.",
-    };
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    }
   }
 
-  revalidatePath("/dashboard/invoices");
-  redirect("/dashboard/invoices");
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data
+  const amountInCents = amount * 100
+  const date = new Date().toISOString().split('T')[0]
+
+  try {
+    await console.log(customerId, status, amountInCents, date)
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    }
+  }
+
+  // Revalidate the cache for the invoices page and redirect the user.
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
 }
 
 // Use Zod to update the expected types
-const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true })
 
 export async function updateInvoice(id: string, formData: FormData) {
   const { customerId, amount, status } = UpdateInvoice.parse({
-    customerId: formData.get("customerId"),
-    amount: formData.get("amount"),
-    status: formData.get("status"),
-  });
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  })
 
-  const amountInCents = amount * 100;
+  const amountInCents = amount * 100
 
   try {
-    await console.log(formData);
+    await console.log(formData)
   } catch (error) {
     return {
-      message: "Database Error: Failed to Create Invoice.",
-    };
+      message: 'Database Error: Failed to Create Invoice.',
+    }
   }
 
-  revalidatePath("/dashboard/invoices");
-  redirect("/dashboard/invoices");
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
 }
 
 export async function deleteInvoice(id: string) {
   try {
-    await console.log(id);
-    return { message: "Deleted Invoice." };
+    await console.log(id)
+    return { message: 'Deleted Invoice.' }
   } catch (error) {
-    return { message: "Database Error: Failed to Delete Invoice." };
+    return { message: 'Database Error: Failed to Delete Invoice.' }
   }
 }
