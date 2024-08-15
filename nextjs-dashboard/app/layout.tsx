578212112ad8b82bac52e2ff228d0c07c00a094b
Nhat Cong Duong <nhat.duong@asnet.com.vn>
2024-08-15 14:52:51 +0700

    Adding metadata

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,14 +1,20 @@
-import "@/app/ui/global.css";
-import { inter } from "@/app/ui/fonts";
+import '@/app/ui/global.css'
+import { inter } from '@/app/ui/fonts'
+import { Metadata } from 'next'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://nextjs14-learning-two.vercel.app/'),
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
